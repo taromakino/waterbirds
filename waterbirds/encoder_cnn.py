616dc162d1fde65ac6358aa47181839ac4e26bb2
makinot1 <makinot1@gene.com>
2023-12-30 22:22:52 -0800

    ImageNet pretrained

--- waterbirds/encoder_cnn.py
+++ waterbirds/encoder_cnn.py
@@ -8,7 +8,7 @@ IMG_ENCODE_SIZE = 1024
 class EncoderCNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.net = densenet121()
+        self.net = densenet121(weights='IMAGENET1K_V1')
         del self.net.classifier
         self.net.classifier = nn.Identity()
 
