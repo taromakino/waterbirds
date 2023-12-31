a040b3a620a7c3320ea5b16cd2d68f15114fffee
makinot1 <makinot1@gene.com>
2023-12-30 22:23:47 -0800

    ImageNet normalize

--- waterbirds/encoder_cnn.py
+++ waterbirds/encoder_cnn.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torchvision.transforms as transforms
 from torchvision.models.densenet import densenet121
 
 
@@ -11,6 +12,7 @@ class EncoderCNN(nn.Module):
         self.net = densenet121(weights='IMAGENET1K_V1')
         del self.net.classifier
         self.net.classifier = nn.Identity()
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     def forward(self, x):
-        return self.net(x)
+        return self.net(self.normalize(x))
