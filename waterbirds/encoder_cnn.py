fa28bf423003ce0e5a349a96409840f8f696f84c
makinot1 <makinot1@gene.com>
2024-01-01 21:04:19 -0800

    Freeze DenseNet121

--- waterbirds/encoder_cnn.py
+++ waterbirds/encoder_cnn.py
@@ -12,6 +12,7 @@ class EncoderCNN(nn.Module):
         self.net = densenet121(weights='IMAGENET1K_V1')
         del self.net.classifier
         self.net.classifier = nn.Identity()
+        self.net.freeze()
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     def forward(self, x):
