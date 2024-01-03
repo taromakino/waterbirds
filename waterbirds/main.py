b209592919060dd9cd089d56aaa5d64efd07da59
makinot1 <makinot1@gene.com>
2024-01-03 15:16:16 -0800

    Minor changes

--- waterbirds/main.py
+++ waterbirds/main.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     parser.add_argument('--h_sizes', nargs='+', type=int, default=[256, 256])
     parser.add_argument('--y_mult', type=float, default=1)
     parser.add_argument('--beta', type=float, default=1)
-    parser.add_argument('--reg_mult', type=float, default=1e-5)
+    parser.add_argument('--prior_reg_mult', type=float, default=1e-5)
     parser.add_argument('--init_sd', type=float, default=1e-3)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--weight_decay', type=float, default=1e-5)
