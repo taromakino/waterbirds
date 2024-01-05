18a50ed5ee97f09a74620e3b1aa0611208fd684b
makinot1 <makinot1@gene.com>
2024-01-04 19:06:16 -0800

    SkipMLP classifier

--- waterbirds/erm.py
+++ waterbirds/erm.py
@@ -4,14 +4,19 @@ import pytorch_lightning as pl
 from encoder_cnn import IMG_ENCODE_SIZE, EncoderCNN
 from torch.optim import AdamW
 from torchmetrics import Accuracy
+from utils.nn_utils import SkipMLP
 
 
 class ERM(pl.LightningModule):
-    def __init__(self, lr, weight_decay):
+    def __init__(self, z_size, h_sizes, lr, weight_decay):
         super().__init__()
         self.save_hyperparameters()
         self.cnn = EncoderCNN()
-        self.fc = nn.Linear(IMG_ENCODE_SIZE, 1)
+        self.classifier = nn.Sequential(
+            nn.Linear(IMG_ENCODE_SIZE, z_size),
+            nn.LeakyReLU(),
+            SkipMLP(z_size, h_sizes, 1)
+        )
         self.lr = lr
         self.weight_decay = weight_decay
         self.val_acc = Accuracy('binary')
@@ -20,7 +25,7 @@ class ERM(pl.LightningModule):
     def forward(self, x, y, e):
         batch_size = len(x)
         x = self.cnn(x).view(batch_size, -1)
-        y_pred = self.fc(x).view(-1)
+        y_pred = self.classifier(x).view(-1)
         return y_pred, y
 
     def training_step(self, batch, batch_idx):
